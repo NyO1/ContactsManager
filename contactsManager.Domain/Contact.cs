dde3a1f98519bd15755f9cc975c24f47a0ff265c
Andrea Schisani <andrea.schisani@researchandmarkets.com>
2016-11-28 17:20:48 +0000

    made changes

---
+++ contactsManager.Domain/Contact.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace contactsManager.Domain
+{
+    public class Contact
+    {
+        public virtual int Id { get; set; }
+        public virtual string FirstName { get; set; }
+        public virtual string LastName { get; set; }
+        public virtual string PhoneNumber { get; set; }
+        public virtual string Email { get; set; }
+        public virtual string PhotoUrl { get; set; }
+
+
+        public string FullName
+        {
+            get { return FirstName + " " + LastName; }
+        }
+    }
+}
---
+++ contactsManager.Domain/IContactDataSource.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace contactsManager.Domain
+{
+    public interface IContactDataSource
+    {
+        IQueryable<Contact> Contacts { get; }
+
+        void Save();
+
+        void Add(Contact c);
+    }
+
+
+
+
+    
+}
